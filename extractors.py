1a00c4ceaf30ef1093da7c7e25081988b053d90c
Rick Barber <barber5@illinois.edu>
2019-02-02 00:52:21 -0600

    refactoring

--- extractors.py
+++ extractors.py
@@ -1,5 +1,13 @@
 from urlparse import urlparse, urljoin
 
+
+# The logic of CrawlSrategy.crawl() will visit a landing page, load it and then call the extractor function
+# used to construct the CrawlStrategy object. It will call this function with a selenium webdriver as an argument
+# any extractor function can use this webdriver to find elements of the DOM which contain links the crawler should
+# visit.
+# You must return a list of WebElements which have an 'href' attribute
+# as the crawler will iterate through this list and construct a list of links to visit
+# by asking each of the WebElements returned by this function for the attribute href
 def get_reddit_stories(webdriver):
     location = webdriver.current_url
     result = []
