f104f6756d9b7fe1d1df480e96148e6b65d389d7
Rick Barber <barber5@illinois.edu>
2019-02-06 17:16:41 -0600

    updating time of day restrictions

--- Scheduler.py
+++ Scheduler.py
@@ -1,10 +1,16 @@
 import time, datetime
+from random import shuffle
 from barber5 import setup_barber5_reddit
-
+from nc7 import setup_nick_irl, setup_ycomb
 
 
 if __name__ == "__main__":
-    crawl_strategies = setup_barber5_reddit()
+    crawl_strategies = []
+    crawl_strategies.extend(setup_ycomb())
+    crawl_strategies.extend(setup_barber5_reddit())
+    crawl_strategies.extend(setup_nick_irl())
+    shuffle(crawl_strategies)
+
     while True:
         for cs in crawl_strategies:
             if cs.can_crawl():
